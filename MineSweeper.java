de768e332c1fceb42578fc1ef0d9822d33a15b14
tephron4 <tobiasephron@gmail.com>
2019-11-08 17:14:49 -0500

    Starting to make MineSweeper game

---
+++ MineSweeper.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class MineSweeper here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MineSweeper
+{
+    private String[][] fieldForPlay;
+    private String[][] fieldWithBombs;
+    Human mh = new Human();
+    
+    public MineSweeper(Human mh){
+        this.mh = mh;
+        this.fieldForPlay = fieldForPlay;
+        this.fieldWithBombs = fieldWithBombs;
+    }
+    
+    public void play(){
+        
+    }
+    
+    
+}
