e1e17496e82d47667aaabf4f6678aa9a95b843a5
tephron4 <tobiasephron@gmail.com>
2019-11-23 12:30:17 -0500

    Fixed printField and initializeBombSpots methods; added lives variable and placesArround method; Finished done, placesArround, and updateField methods.

--- MineSweeper.java
+++ MineSweeper.java
@@ -16,7 +16,9 @@ public class MineSweeper
     Human mh = new Human();
     /** A variable for the number of bombs */
     private int bombs = 10;
-    
+    /** A variable to keep track of if they've hit a bomb or not*/
+    private int lives = 1;
+
     /**
      * A constructor method for the MineSweeper class
      * 
@@ -33,9 +35,11 @@ public class MineSweeper
      */
     public void play(){
         // main method that runs through playing the game
-        //this.initializeField();
+        //his.initializeField();
+        //this.initializeBombSpots();
         //this.printField();
         //this.flag(new int[]{4,3});
+        //this.addBombSpots();
         //this.printField();
     }
     
@@ -45,15 +49,25 @@ public class MineSweeper
     public void initializeBombSpots(){
         // method to add to the list of where the 10 bombs are on the board
         int bombsAdded = 0;
-        while(bombsAdded < 10){
-            int x = (int) (Math.random() * 10.0);
-            int y = (int) (Math.random() * 10.0);
+        while(this.bombSpots.size() < 10){
+            int x = (int) (Math.random() * 8.0);
+            int y = (int) (Math.random() * 8.0);
             int[] spot = new int[]{x,y};
-            if(this.bombSpots.contains(spot)){
-                continue;
+            boolean alreadyIn = false;
+            for(int[] check: this.bombSpots){
+                if(check[0] == spot[0] && check[1] == spot[1]){
+                    alreadyIn = true;
+                    break;
+                }
             }
-            this.bombSpots.add(spot);
-            bombsAdded++;
+            if(alreadyIn) continue;
+            else{
+                this.bombSpots.add(spot);
+            }
+        }
+        System.out.println(this.bombSpots.size());
+        for(int[] spot: this.bombSpots){
+            System.out.println("(" + spot[0] + "," + spot[1] + ") , ");
         }
     }
     
@@ -76,23 +90,23 @@ public class MineSweeper
         System.out.println("");
         System.out.println("  0 1 2 3 4 5 6 7 8");
         System.out.println("  - - - - - - - - -");
-        System.out.println("5|" + this.field[5][5] + "|" + this.field[5][1] + "|" + this.field[5][2] + "|" +
-                            this.field[5][3] + "|" + this.field[5][4] + "|" + this.field[5][5] + "|" + this.field[5][6] +
-                            "|" + this.field[5][7] + "|" + this.field[5][8] + "|");
+        System.out.println("0|" + this.field[0][0] + "|" + this.field[0][1] + "|" + this.field[0][2] + "|" +
+                            this.field[0][3] + "|" + this.field[0][4] + "|" + this.field[0][5] + "|" + this.field[0][6] +
+                            "|" + this.field[0][7] + "|" + this.field[0][8] + "|");
         System.out.println("  - - - - - - - - -");
-        System.out.println("1|" + this.field[1][5] + "|" + this.field[1][1] + "|" + this.field[1][2] + "|" +
+        System.out.println("1|" + this.field[1][0] + "|" + this.field[1][1] + "|" + this.field[1][2] + "|" +
                             this.field[1][3] + "|" + this.field[1][4] + "|" + this.field[1][5] + "|" + this.field[1][6] +
                             "|" + this.field[1][7] + "|" + this.field[1][8] + "|");
         System.out.println("  - - - - - - - - -");
-        System.out.println("2|" + this.field[2][5] + "|" + this.field[2][1] + "|" + this.field[2][2] + "|" +
+        System.out.println("2|" + this.field[2][0] + "|" + this.field[2][1] + "|" + this.field[2][2] + "|" +
                             this.field[2][3] + "|" + this.field[2][4] + "|" + this.field[2][5] + "|" + this.field[2][6] +
                             "|" + this.field[2][7] + "|" + this.field[2][8] + "|");
         System.out.println("  - - - - - - - - -");
-        System.out.println("3|" + this.field[3][5] + "|" + this.field[3][1] + "|" + this.field[3][2] + "|" +
+        System.out.println("3|" + this.field[3][0] + "|" + this.field[3][1] + "|" + this.field[3][2] + "|" +
                             this.field[3][3] + "|" + this.field[3][4] + "|" + this.field[3][5] + "|" + this.field[3][6] +
                             "|" + this.field[3][7] + "|" + this.field[3][8] + "|");
         System.out.println("  - - - - - - - - -");
-        System.out.println("4|" + this.field[4][5] + "|" + this.field[4][1] + "|" + this.field[4][2] + "|" +
+        System.out.println("4|" + this.field[4][0] + "|" + this.field[4][1] + "|" + this.field[4][2] + "|" +
                             this.field[4][3] + "|" + this.field[4][4] + "|" + this.field[4][5] + "|" + this.field[4][6] +
                             "|" + this.field[4][7] + "|" + this.field[4][8] + "|");
         System.out.println("  - - - - - - - - -");
@@ -141,7 +155,104 @@ public class MineSweeper
      * @param place an integer array with the coordinates to be move to
      */
     public void updateField(int[] place){
+        // first check if place is where a bomb is
+        if(this.bomb(place)){
+            this.addBombSpots();
+            this.lives--;
+            return;
+        }
+        // otherwise get the number of bombs arround place
+        int bombsArr = this.bombsArround(place);
+        // if their are no bombs around it (bombsArr == 0)
+        //    put "0" on the field and update the field for
+        //    all surrounding places
+        if(bombsArr == 0){
+            this.field[place[0]][place[1]] = "0";
+            ArrayList<int[]> placesArr = this.placesArround(place);
+            Iterator<int[]> itr = placesArr.iterator();
+            while(itr.hasNext()){
+                this.updateField(itr.next());
+            } 
+        }
+        // otherwise put the number of bombs arround the given place
+        //      on the field at the given place
+        else{
+            this.field[place[0]][place[1]] = Integer.toString(this.bombsArround(place));
+        }
+    }
 
+    /**
+     * A method to get the spots surrounding a given place
+     *
+     * @param place an integer array with the central coordinates
+     * @return an ArrayList of integer arrays with the coordinates
+     *               of the places arround given place
+     */
+    public ArrayList<int[]> placesArround(int[] place){
+        ArrayList<int[]> acc = new ArrayList<int[]>();
+        if(place[0] == 0){
+            if(place[1] == 0){
+                acc.add(new int[]{0,1});
+                acc.add(new int[]{1,1});
+                acc.add(new int[]{1,0});
+            }
+            else if(place[1] == 8){
+                acc.add(new int[]{0,7});
+                acc.add(new int[]{1,7});
+                acc.add(new int[]{1,8});
+            }
+            else{
+                acc.add(new int[]{0,place[1]-1});
+                acc.add(new int[]{1,place[1]-1});
+                acc.add(new int[]{1,place[1]});
+                acc.add(new int[]{1,place[1]+1});
+                acc.add(new int[]{0,place[1]+1});
+            }
+        }
+        else if(place[0] == 8){
+            if(place[1] == 0){
+                acc.add(new int[]{7,0});
+                acc.add(new int[]{7,1});
+                acc.add(new int[]{8,1});
+            }
+            else if(place[1] == 8){
+                acc.add(new int[]{8,7});
+                acc.add(new int[]{7,7});
+                acc.add(new int[]{7,8});
+            }
+            else{
+                acc.add(new int[]{8,place[1]-1});
+                acc.add(new int[]{7,place[1]-1});
+                acc.add(new int[]{7,place[1]});
+                acc.add(new int[]{7,place[1]+1});
+                acc.add(new int[]{8,place[1]+1});
+            }
+        }
+        else if(place[1] == 0){
+            acc.add(new int[]{place[0]-1,0});
+            acc.add(new int[]{place[0]-1,1});
+            acc.add(new int[]{place[0],1});
+            acc.add(new int[]{place[0]+1,1});
+            acc.add(new int[]{place[0]+1,0});
+        }
+        else if(place[1] == 8){
+            acc.add(new int[]{place[0]-1,8});
+            acc.add(new int[]{place[0]-1,7});
+            acc.add(new int[]{place[0],7});
+            acc.add(new int[]{place[0]+1,7});
+            acc.add(new int[]{place[0]+1,8});
+        }
+        else{
+            acc.add(new int[]{place[0],place[1]-1});
+            acc.add(new int[]{place[0]-1,place[1]-1});
+            acc.add(new int[]{place[0]-1,place[1]});
+            acc.add(new int[]{place[0]-1,place[1]+1});
+            acc.add(new int[]{place[0],place[1]+1});
+            acc.add(new int[]{place[0]+1,place[1]+1});
+            acc.add(new int[]{place[0]+1,place[1]});
+            acc.add(new int[]{place[0]+1,place[1]-1});
+        }
+        return acc;
     }
 
     /**
